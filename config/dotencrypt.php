7cea4407920ff45ad6f8991a7f04b83df2656c5a
Andrea Zorzi <andreaz@europlan.it>
2026-04-02 17:33:35 +0200

    Fix default env files config

--- config/dotencrypt.php
+++ config/dotencrypt.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'env_files' => explode(",", env('DOTENCRYPT_ENV_FILES', [".env,.env.production"])),
+    'env_files' => explode(",", env('DOTENCRYPT_ENV_FILES', ".env,.env.production")),
     'strong_password' => env('DOTENCRYPT_STRONG_PASSWORD', true)
 ];
